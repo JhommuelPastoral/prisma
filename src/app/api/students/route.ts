effa3ba9dcdef32d2dbe5c04f00713dfae6414fe
Jhommuel Pastoral <jhommuelpastoral9@gmail.com>
2025-10-23 11:38:20 +0800

    pagination, skipping, filltering, and sorting

--- src/app/api/students/route.ts
+++ src/app/api/students/route.ts
@@ -1,55 +1,148 @@
+
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 
+// Create Many Users
+
+type Student ={
+  name: string;
+  age: number;
+  course: string;
+  year: number;
+  subjects: string[]
+}
+
 export async function POST(req:Request) {
   try {
-    const {name, age, course, year, subjects} = await req.json();
-    if(!name || !age || !course || !year || !subjects) {
-      return NextResponse.json({message:"All fields are required"}, {status:400});
-    };
-    const existUser = await prisma.student.findFirst({
-      where: {
-        name,
-        age,
-        course,
-        year,
-        subjects:{
-          equals:subjects
-        }
+    const {students} : {students: Student[]} = await req.json();
+    if(!students) return NextResponse.json({Successful: false, message:"All fields are required"}, {status:400});
+
+    const existUser = await prisma.student.findMany({
+      where:{
+        OR: students.map((student) => ({
+          name: student.name,
+          age: student.age,
+          course: student.course,
+          year: student.year,
+          subjects: {hasEvery: student.subjects}
+        })),
       }
     });
-    if(existUser){
-      return NextResponse.json({message:"User already exists"}, {status:400});
-    };
-    const user = await prisma.student.create({
-      data: {
-        name,
-        age,
-        course,
-        year,
-        subjects
-      }
+    if(existUser.length > 0) return NextResponse.json({Successful: false, message:"User already exists"}, {status:400});
+    const user = await prisma.student.createMany({
+      data: students
     });
-    return NextResponse.json({message:"User created", user}, {status:200});
+    return NextResponse.json({Successful: true, message:"Users created", user}, {status:200});
+
+
   } catch (error) {
     console.log("Create user error", error);
     return NextResponse.json({message:"Something went wrong"}, {status:500});
   }
 }
 
-export async function GET() {
+
+// Create User
+
+// export async function POST(req:Request) {
+//   try {
+//     const {name, age, course, year, subjects} = await req.json();
+//     if(!name || !age || !course || !year || !subjects) {
+//       return NextResponse.json({message:"All fields are required"}, {status:400});
+//     };
+//     const existUser = await prisma.student.findFirst({
+//       where: {
+//         name,
+//         age,
+//         course,
+//         year,
+//         subjects:{
+//           equals:subjects
+//         }
+//       }
+//     });
+//     if(existUser){
+//       return NextResponse.json({message:"User already exists"}, {status:400});
+//     };
+//     const user = await prisma.student.create({
+//       data: {
+//         name,
+//         age,
+//         course,
+//         year,
+//         subjects
+//       }
+//     });
+//     return NextResponse.json({message:"User created", user}, {status:200});
+//   } catch (error) {
+//     console.log("Create user error", error);
+//     return NextResponse.json({message:"Something went wrong"}, {status:500});
+//   }
+// }
+
+// Basic Get Query
+
+// export async function GET() {
+//   try {
+//     const users = await prisma.student.findMany({
+//       where: {
+//         age:{gte:20},
+//         subjects: {hasSome:["AI"]}
+//       }
+//     });  
+//     return NextResponse.json({users}, {status:200});
+//   } 
+//   catch (error) {
+//     console.log("Get users error", error);
+//   }
+// }
+
+// Get Query with filters
+
+// export async function GET(req:Request) {
+//   try {
+//     const {searchParams} = new URL(req.url);
+//     const course = searchParams.get("course");
+//     const sort = searchParams.get("sort");
+//     if(!course || !sort) return NextResponse.json({message:"All fields are required"}, {status:400});
+//     const users = await prisma.student.findMany({
+//       where:{course},
+//       orderBy:{
+//         age: sort === "desc" ? "desc" : "asc"
+//       }
+//     });
+
+//     return NextResponse.json({Succesful: true, users}, {status:200});
+
+//   } 
+//   catch (error) {
+//     console.log("Get users error", error);
+//     return NextResponse.json({message:"Something went wrong"}, {status:500});
+//   }
+// }
+
+// Pagination 
+export async function GET(req:Request) {
   try {
-    const users = await prisma.student.findMany({
-      where: {
-        age:{gte:20},
-        subjects: {hasSome:["AI"]}
+    const {searchParams} = new URL(req.url);
+    const page =  Number(searchParams.get("page") || "1" );
+    const limit =  Number(searchParams.get("limit") || "10");
+
+    const user = await prisma.student.findMany({
+      skip: (page - 1) * limit,
+      take: limit,
+      orderBy: {
+        name: "asc"
       }
-    });  
-    return NextResponse.json({users}, {status:200});
+    });
+    return NextResponse.json({Succesful: true, user}, {status:200});
+
+
   } 
   catch (error) {
     console.log("Get users error", error);
+    return NextResponse.json({message:"Something went wrong"}, {status:500});
   }
 }
 

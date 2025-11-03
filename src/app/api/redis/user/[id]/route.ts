4366a3442cf40bbae160778b376362c9cc71b49f
Jhommuel Pastoral <jhommuelpastoral9@gmail.com>
2025-11-04 06:49:26 +0800

    added comment

--- src/app/api/redis/user/[id]/route.ts
+++ src/app/api/redis/user/[id]/route.ts
@@ -7,11 +7,14 @@ export async function GET(req:Request, {params}: {params: Promise<{id: string}>}
     const {id} = await params;
     const cachedKey= `user:${id}`;
 
+    // Get Cached
     const cached = await redis.get(cachedKey);
     if(cached) return NextResponse.json({cached, message:"User fetched from cached"}, {status:200});
 
     const user = await prisma.redisUser.findUnique({where:{id}});
+
     if(!user) return NextResponse.json({message:"User not found", successful: false}, {status:404});
+    // Set Cache
     await redis.set(cachedKey, JSON.stringify(user));
     return NextResponse.json({message:"User fetched", successful: true}, {status:200});
   } catch (error) {
